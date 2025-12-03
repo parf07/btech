d24982501f3501d3431d5f5c16e634118a92a7fa
parf07 <parfaiteniyonsaba1@gmail.com>
2025-12-03 10:29:48 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files from 'pages' directory
+app.use(express.static(path.join(__dirname, 'pages')));
+
+// Routes for each page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'home.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'about.html'));
+});
+
+app.get('/contactus', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'contactus.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
